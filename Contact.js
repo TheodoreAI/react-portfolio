8ccf58325229a3fea27aeb9e1c3e8b635c01c91a
Mateo Estrada <mateoej12@gmail.com>
2021-09-06 17:16:41 -0700

    Added new functionality.

--- Contact.js
+++ Contact.js
@@ -56,7 +56,7 @@ export default function Contact() {
                   EMAIL
                 </h2>
                 <a className="text-indigo-400 leading-relaxed">
-                  reedbarger@email.com
+                  testing@email.com
                 </a>
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                   PHONE
